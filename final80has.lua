b8b3beb06d751d6ffe76ad4d4a31e76eb2f96dd0
NaturalNi13 <naturalidiot1234@gmail.com>
2024-11-11 15:03:08 -0600

    Update final80has.lua

--- final80has.lua
+++ final80has.lua
@@ -9,12 +9,14 @@ while true do
     -- Check if the current map is "hide and seek"
     if workspace.game.currentMap:getChildren()[1].Name == "hide and seek" then
         -- Play custom sound if ingameTimer is less than or equal to 80
-        if game:GetService("ReplicatedStorage").settingsDisplay.ingameTimer.Value <= 80 then
-            local SoundId = "rbxassetid://13733949175"  -- Replace with your actual ID
-                game:GetService("SoundService").mainSounds.current.SoundId = SoundId
-                game:GetService("SoundService").mainSounds.current.PlaybackSpeed = 1
+        if game:GetService("ReplicatedStorage").settingsDisplay.ingameTimer then
+            if game:GetService("ReplicatedStorage").settingsDisplay.ingameTimer.Value <= 80 then
+                local SoundId = "rbxassetid://13733949175"  -- Replace with your actual ID
+                    game:GetService("SoundService").mainSounds.current.SoundId = SoundId
+                    game:GetService("SoundService").mainSounds.current.PlaybackSpeed = 1
+            end
         end
-    end
+   end
     task.wait(0.1)
 end
 end
