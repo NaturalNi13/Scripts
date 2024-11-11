57290079bcd4f591532ac1390d94ec24540fa0f9
NaturalNi13 <naturalidiot1234@gmail.com>
2024-11-11 14:52:52 -0600

    Create final80has.lua

---
+++ final80has.lua
@@ -0,0 +1,19 @@
+local checkEncrypted = loadstring(game:HttpGet("https://raw.githubusercontent.com/NaturalNi13/Scripts/refs/heads/main/encrypter.lua"))()
+local result = checkEncrypted(owner)
+
+ if result then
+local canPlay = true
+
+while true do
+    -- Check if the current map is "hide and seek"
+    if workspace.game.currentMap:getChildren()[1].Name == "hide and seek" then
+        -- Play custom sound if ingameTimer is less than or equal to 80
+        if game:GetService("ReplicatedStorage").settingsDisplay.ingameTimer.Value <= 80 then
+            local SoundId = "rbxassetid://13733949175"  -- Replace with your actual ID
+                game:GetService("SoundService").mainSounds.current.SoundId = SoundId
+                game:GetService("SoundService").mainSounds.current.PlaybackSpeed = 1
+        end
+    end
+    task.wait(0.1)
+end
+end
